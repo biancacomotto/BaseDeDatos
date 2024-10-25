955ba7966bc268d02c0005ecbabd29fa8fcc9b5c
catalinapereyra <cpereyra@mail.austral.edu.ar>
2024-10-25 10:39:06 -0300

    boton listar

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 //const db = new sqlite3.Database('./movies.db'); //path
 
-//cataaaaaaaa
+//holaaa roco
 
 // Serve static files from the "views" directory
 app.use(express.static('views'));
