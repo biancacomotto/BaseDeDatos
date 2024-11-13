c8abed3239a472f76144ecde0669fc8f75bb5a22
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2024-11-13 12:05:11 -0300

    .

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const methodOverride = require('method-override');
 const bcrypt = require('bcrypt');
 const { LocalStorage } = require('node-localstorage');
 
-//HOLA PP
 
 const localStorage = new LocalStorage('./scratch');
 const app = express();
